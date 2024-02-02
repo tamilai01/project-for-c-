cde5c4acf58584c264a6b34300ceb45a36cce6f5
Tamil V <Tamilai01@brightskills.com>
2024-02-02 12:04:58 +0530

    c++ files\

---
+++ cplusplus/cplus/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+namespace student
+{
+
+
+
+
+
+/*
+int main()
+{
+
+
+    string name;
+    /*
+    cout<<"Enter yr name:"<<endl;
+    cin>>name;
+    cout<<name<<endl;
+    fflush(stdin);
+    cout<<"enter your name:"<<endl;
+    getline(cin,name);
+    cout<<name<<endl;
+}
+
+int main()
+{
+
+    int n;
+    string str;
+    cout<<"enter the two number"<<endl;
+    cin >> n;
+    fflush(stdin);
+
+    getline(cin, str);
+    cout << n << endl << str << endl;
+}
+
+
+
+
+
+string firstname;
+string lastname;
+cin>>firstname>>lastname;
+cout<<firstname +" "+ lastname<<endl;
+string fullname=firstname.append(lastname);
+cout<<fullname<<endl;
+
+lastname.push_back('T');
+cout<<lastname<<endl;
+firstname.push_back('tt');
+cout<<fullname;
+cout<<firstname+" "+lastname>endl;
+cout<<fullname.length();
+string fullname;
+string name="name";
+
+//lenght,size
+cout<<"enter your name"<<endl;
+cin>>fullname;
+cout<<"lenght of the string is:";
+cout<<fullname.length()<<endl;
+cout<<"size of the string is"<<endl;
+cout<<fullname.size()<<endl;
+
+//insert
+fflush(stdin);
+string fullname;
+cin>>fullname;
+fullname.insert(0,"tl");
+cout<<fullname;
+//
+
+*/
+
+
+
+
+    string name="name";
+}
+int main()
+{
+
+    string name="tamil";
+    cout<<name<<endl;
+    string name1="chithu";
+    cout<<name1<<endl;
+    return 0;
+}
+
+
